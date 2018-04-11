e29265ed4bab1efede0554f87fb6bab86f246ccf
SergeiMeza <sergeimeza@gmail.com>
2018-04-11 21:32:09 +0900

    setup

--- RealmBackgroundQueue/AppDelegate.swift
+++ RealmBackgroundQueue/AppDelegate.swift
@@ -7,40 +7,71 @@
 //
 
 import UIKit
+import RealmSwift
+
+let realmQueue = DispatchQueue.init(label: "Realm")
+
+extension Notification.Name {
+    static var newUsersAdded = Notification.Name.init("NewUsersAdded")
+}
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+        
+        realmQueue.async {
+            autoreleasepool {
+                let realm = try! Realm()
+                try! realm.write {
+                    for i in 1...100_000 {
+                        let user = DBUser()
+                        user.objectId = "\(i)"
+                        user.name = "NAME\(i)"
+                        user.age = "AGE\(i)"
+                        realm.add(user, update: true)
+                    }
+                }
+                NotificationCenter.default.post(name: .newUsersAdded, object: nil)
+            }
+        }
+        
+        let vc = ViewController()
+        let nvc = UINavigationController.init(rootViewController: vc)
+        nvc.title = "Realm UI"
+        let tbc = UITabBarController.init()
+        tbc.viewControllers = [nvc]
+        window?.rootViewController = tbc
+        
         return true
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
 }
 
+@objcMembers class DBUser: Object {
+    
+    dynamic var objectId = ""
+    dynamic var name = ""
+    dynamic var age = ""
+    
+    override static func primaryKey() -> String? {
+        return "objectId"
+    }
+}
+
+struct User {
+    let objectId: String
+    let name: String
+    let age: String
+    
+    init(objectId: String, name: String, age: String) {
+        self.objectId = objectId
+        self.name = name
+        self.age = age
+    }
+}
--- RealmBackgroundQueue/ViewController.swift
+++ RealmBackgroundQueue/ViewController.swift
@@ -7,19 +7,81 @@
 //
 
 import UIKit
+import RealmSwift
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
+    var items =  [User]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.backgroundColor = UIColor.init(white: 0.95, alpha: 1)
+        title = "Realm UI"
+        tableView.register(RealmCell.self, forCellReuseIdentifier: String.init(describing: RealmCell.self))
+        tableView.rowHeight = 50
+        tableView.tableFooterView = nil
+        NotificationCenter.default.addObserver(self, selector: #selector(reloadItems), name: .newUsersAdded, object: nil)
+        
+        reloadItems()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self, name: .newUsersAdded, object: nil)
     }
-
-
+    
+    @objc func reloadItems() {
+        DispatchQueue.init(label: "background").async {
+            autoreleasepool {
+                let realm = try! Realm()
+                let items = realm.objects(DBUser.self)
+                let minIndex = min(20, items.count)
+                for i in 0...minIndex-1 {
+                    let item = items[i]
+                    let user = User.init(objectId: item.objectId, name: item.name, age: item.age)
+                    self.items.append(user)
+                }
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let item = items[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: String.init(describing: RealmCell.self), for: indexPath) as! RealmCell
+        cell.textLabel?.text = item.name
+        cell.detailTextLabel?.text = item.age
+        return cell
+    }
 }
 
+class RealmCell: UITableViewCell {
+    
+    lazy var label: UILabel = {
+        let label = UILabel.init()
+        label.text = "hello world"
+        return label
+    }()
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+        setupSubviews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupSubviews()
+    }
+    
+    func setupSubviews() {
+        contentView.backgroundColor = .white
+    }
+    
+}
